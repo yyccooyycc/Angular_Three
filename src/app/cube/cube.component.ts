aee5c17c6ed877eb61c628a24b9b9dd848743ed1
oliviah11 <yycc11.tt@gmail.com>
2023-12-25 14:25:12 +0800

    cube with rickyAndMorty

--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -7,8 +7,9 @@ import * as THREE from 'three';
   standalone: true,
 })
 export class CubeComponent implements OnInit, AfterViewInit {
-  ngAfterViewInit(): void {
-    throw new Error('Method not implemented.');
+  ngAfterViewInit() {
+    this.createScene();
+    this.startRendererLoop();
   }
   ngOnInit(): void {
     throw new Error('Method not implemented.');
@@ -16,8 +17,8 @@ export class CubeComponent implements OnInit, AfterViewInit {
   @ViewChild('canvas') private canvasRef!: ElementRef;
 
   //Cube properties
-  @Input() public rotationSpeedX: number = 0.05;
-  @Input() public rotationSpeedY: number = 0.01;
+  @Input() public rotationSpeedX: number = 0.005;
+  @Input() public rotationSpeedY: number = 0.005;
   @Input() public size: number = 200;
   @Input() public texture: string = '/assets/Ricky_Cube.jpeg';
 
@@ -35,7 +36,7 @@ export class CubeComponent implements OnInit, AfterViewInit {
   }
 
   private loader = new THREE.TextureLoader();
-  private geometry = new THREE.BoxGeometry(1, 1, 1);
+  private geometry = new THREE.BoxGeometry(10, 10, 10);
   private material = new THREE.MeshBasicMaterial({
     map: this.loader.load(this.texture),
   });
@@ -43,4 +44,41 @@ export class CubeComponent implements OnInit, AfterViewInit {
   private cube: THREE.Mesh = new THREE.Mesh(this.geometry, this.material);
   private renderer!: THREE.WebGLRenderer;
   private scene!: THREE.Scene;
+
+  private createScene() {
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x000000);
+    this.scene.add(this.cube);
+
+    let aspectRatio = this.getAspectRatio();
+    this.camera = new THREE.PerspectiveCamera(
+      this.fieldOfView,
+      aspectRatio,
+      this.nearClippingPlane,
+      this.farClippingPlane
+    );
+    this.camera.position.z = this.cameraZ;
+  }
+
+  private getAspectRatio(){
+    return this.canvas.clientWidth/this.canvas.clientHeight
+  }
+
+  private animateCube() {
+    this.cube.rotation.x += this.rotationSpeedX;
+    this.cube.rotation.y += this.rotationSpeedY;
+  }
+
+  private startRendererLoop() {
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
+
+    let component: CubeComponent = this;
+    (function render() {
+      requestAnimationFrame(render);
+      component.animateCube();
+      component.renderer.render(component.scene, component.camera);
+    })();
+  }
 }
